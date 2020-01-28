9b6e160a5d74b30738ee51983e9dbc3bd4b222b8
LabinotVila <33487958+LabinotVila@users.noreply.github.com>
2020-01-28 11:53:45 +0100

    Added commenting and renamed some variables

--- src/main/scala/NQueens.scala
+++ src/main/scala/NQueens.scala
@@ -2,86 +2,104 @@ import scala.util.Try
 
 object NQueens {
 
-	def isHorizontallyGood(queens: Array[Array[Int]], x: Int): Boolean = !queens(x).contains(1)
+	/** Checks if the value to be placed is horizontally valid */
+	def isHorizontallyGood(board: Array[Array[Int]], x: Int): Boolean = !board(x).contains(1)
 
-	def isVerticallyGood(queens: Array[Array[Int]], y: Int): Boolean = {
-		queens.foreach(row => if (row(y).equals(1)) return false)
-
-		true
-	}
-
-	def isSlopeGood(queens: Array[Array[Int]], x: Int, y: Int): Boolean = {
-		for (value <- x to queens.length - 1) {
-			val tryRightDiagonal = Try(queens(value)(y + value - x)).getOrElse(null)
-			val tryLeftDiagonal = Try(queens(value)(y - value + x)).getOrElse(null)
-
-			if (tryRightDiagonal == 1 || tryLeftDiagonal == 1) return false
-		}
-
-		for (value <- (queens.length - 1 - x) to queens.length - 1) {
-			val tryRightDiagonal = Try(queens(queens.length - 1 - value)(y + value - (queens.length - 1 - x))).getOrElse(null)
-			val tryLeftDiagonal = Try(queens(queens.length - 1 - value)(y - value + (queens.length - 1 - x))).getOrElse(null)
-
-			if (tryRightDiagonal == 1 || tryLeftDiagonal == 1) return false
-		}
+	/** Checks if the value to be placed is vertically valid */
+	def isVerticallyGood(board: Array[Array[Int]], y: Int): Boolean = {
+		board.foreach(row => if (row(y).equals(1)) return false)
 
 		true
 	}
 
-	def isPositionedGood(queens: Array[Array[Int]], x: Int, y: Int): Boolean =
-		isHorizontallyGood(queens, x) && isVerticallyGood(queens, y) && isSlopeGood(queens, x, y)
+	/** Checks if the diagonals of the value to be placed are valid */
+	def isSlopeGood(board: Array[Array[Int]], x: Int, y: Int): Boolean = {
+		for (value <- x to board.length - 1) {
+			val tryRightDiagonal = Try(board(value)(y + value - x)).getOrElse(null)
+			val tryLeftDiagonal = Try(board(value)(y - value + x)).getOrElse(null)
 
-	def printSolution(queens: Array[Array[Int]]): Unit = {
-		queens.foreach(row => {
-			row.foreach(value => print(value + "  "))
+			if (tryRightDiagonal == 1 || tryLeftDiagonal == 1) return false
+		}
 
-			println()
-		})
+		for (value <- (board.length - 1 - x) to board.length - 1) {
+			val tryRightDiagonal = Try(board(board.length - 1 - value)(y + value - (board.length - 1 - x))).getOrElse(null)
+			val tryLeftDiagonal = Try(board(board.length - 1 - value)(y - value + (board.length - 1 - x))).getOrElse(null)
+
+			if (tryRightDiagonal == 1 || tryLeftDiagonal == 1) return false
+		}
+
+		true
 	}
 
-	def isBoardSolved(queens: Array[Array[Int]]): Boolean = {
+	/** Composes horizontal, vertical and diagonals and checks if the value to be placed is valid */
+	def isPositionedGood(board: Array[Array[Int]], x: Int, y: Int): Boolean =
+		isHorizontallyGood(board, x) && isVerticallyGood(board, y) && isSlopeGood(board, x, y)
+
+	/** Prints the solution of the board */
+	def printSolution(board: Array[Array[Int]]): Unit = {
+		println("Given board has been successfully solved with the following solution: ")
+
+		board.foreach(row => println(row.mkString("  ")))
+	}
+
+	/** Checks if the board is solved */
+	def isBoardSolved(board: Array[Array[Int]]): Boolean = {
 		var queensFound = 0
-		val queensLength = queens.length - 1
+		val boardLength = board.length - 1
 
-		for (x <- 0 to queensLength)
-			for (y <- 0 to queensLength)
-				if (queens(x)(y).equals(1)) queensFound = queensFound + 1
+		for (x <- 0 to boardLength)
+			for (y <- 0 to boardLength)
+				if (board(x)(y).equals(1)) queensFound = queensFound + 1
 
-		if (queensFound != queens.length) return false
+		if (queensFound != board.length) return false
 
-		printSolution(queens)
 		true
 	}
 
-	def Backtrack(queens: Array[Array[Int]], row: Int): Boolean = {
-		if (isBoardSolved(queens)) return true
+	/** Backtracks for solution */
+	def tryBacktrack(board: Array[Array[Int]], row: Int): Boolean = {
+		if (isBoardSolved(board)) {
+			printSolution(board)
 
-		for (x <- 0 to queens.length - 1) {
-			if (isPositionedGood(queens, row, x)) {
-				queens(row)(x) = 1
+			return true
+		}
 
-				if (Backtrack(queens, row + 1))
-					return true
+		for (x <- 0 to board.length - 1) {
+			if (isPositionedGood(board, row, x)) {
+				board(row)(x) = 1
 
-				queens(row)(x) = 0
+				if (tryBacktrack(board, row + 1)) return true
+
+				board(row)(x) = 0
 			}
 		}
 
 		false
 	}
 
-	def main(args: Array[String]): Unit = {
-		val N = 7
-		var queens = Array[Array[Int]]()
+	/** Solve the board as NQueens */
+	def solve(N: Int): Boolean = {
+		var board = Array[Array[Int]]()
 
 		for (_ <- 0 to N - 1) {
 			var arr = Array[Int]()
 
 			for (_ <- 0 to N - 1) arr = arr :+ 0
 
-			queens = queens :+ arr
+			board = board :+ arr
 		}
 
-		Backtrack(queens, 0)
+		tryBacktrack(board, 0)
 	}
+
+	/** Main function */
+	def main(args: Array[String]): Unit = {
+		val N = 7
+
+		println("Trying to solve the board with N = " + N + ".")
+		val isSolvable = solve(N)
+
+		if (!isSolvable) println("Given board has no solution!")
+	}
+
 }
